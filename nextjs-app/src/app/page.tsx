82e91df05dc49b900608ab7dfe5c74d60ef3857b
Tahlil <bsse0803@iit.du.ac.bd>
2024-09-16 23:51:32 +0600

    Add github link

--- nextjs-app/src/app/page.tsx
+++ nextjs-app/src/app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { useEffect, useRef } from "react";
-
+import { ArrowDownTrayIcon } from '@heroicons/react/24/outline';
 export default function Home() {
   const mainRef = useRef<HTMLDivElement | null>(null);
+  
+    const googleDriveLink = "https://drive.google.com/uc?export=download&id=1pzmfSE2MswvHK34GeseVHMjyA7GOeWfZ"; // Replace with your Google Drive file link
 
   useEffect(() => {
     const updateMousePosition = (ev: MouseEvent) => {
@@ -24,7 +26,21 @@ export default function Home() {
       className="h-[75vh] flex items-center justify-center before:pointer-events-none before:fixed before:inset-0 before:z-0 before:bg-[radial-gradient(circle_farthest-side_at_var(--x,_100px)_var(--y,_100px),_rgb(var(--color-primary))_0%,_transparent_16%)] before:opacity-40"
       ref={mainRef}
     >
-      <h1>NEXT JS BOILERPLATE</h1>
+       <div className="flex items-center justify-center">
+            <div className="text-center bg-white/10 p-4 rounded-2xl shadow-lg backdrop-blur-md">
+                <h2 className="text-4xl font-bold text-white mb-6">Download Side Pannel Extension</h2>
+                <a
+                    href={googleDriveLink}
+                    className="btn btn-outline btn-accent btn-lg"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {/* Download Now */}
+                    <ArrowDownTrayIcon className="h-6 w-6 mx-auto" />
+                    
+                </a>
+            </div>
+        </div>
     </main>
   );
 }
